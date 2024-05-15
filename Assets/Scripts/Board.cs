ea29a08d1c94208fabcb65740a75bbceb8f02448
이민형 <lmh4873@gmail.com>
2024-05-15 15:52:04 +0900

    feat: Swap 기능 구현
    - button 위치는 그대로 두고 image만 이동시킨 후 setparent로 버튼에 바뀐 이미지를 붙인다.

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,10 +1,17 @@
+using System;
+using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Board : MonoBehaviour
 {
     public static Board instance = null;
-    public Row[] rows;
 
+    public Row[] rows;
+    private List<Tile> selectedTiles = new List<Tile>();
+
+    private float moveDelay = 0.2f;
     public void Awake()
     {
         if(instance == null)
@@ -15,15 +22,60 @@ public class Board : MonoBehaviour
 
     public void Start()
     {
-        var datas = Resources.LoadAll<Item>("Prefabs/Items");
+        var items = Resources.LoadAll<Item>("Prefabs/Items");
 
-        Debug.Log(datas.Length);
-        for(int y = 0; y < rows.Length; y++)
+        for (int y = 0; y < rows.Length; y++)
         {
             for(int x = 0; x < rows[y].tiles.Length; x++)
             {
-                rows[y].tiles[x].SetTile(x, y, Resources.LoadAll<Item>("Prefabs/Items")[0]);
+                rows[y].tiles[x].SetTile(x, y, items[UnityEngine.Random.Range(0, items.Length)]);
             }
         }
+    }
+
+    public void OnClickTile(Tile _tile)
+    {
+        Debug.Log($"current tile x = {_tile.x}, y = {_tile.y}, listCount = {selectedTiles.Count}");
+
+        if(selectedTiles.Count == 0)
+        {
+            selectedTiles.Add(_tile);
+        }
+        else if(selectedTiles.Count == 1)
+        {
+            if (Array.Exists(selectedTiles[0].checkTiles, x => x == _tile))
+            {
+                Debug.Log("Exist");
+                selectedTiles.Add(_tile);
+                DoSwap(selectedTiles[0], selectedTiles[1]);
+            }
+            else
+            {
+                Debug.Log("Not Exist");
+                selectedTiles.Clear();
+            }
+        }
+    }
+
+    public async void DoSwap(Tile _tile1, Tile _tile2)
+    {
+        Debug.Log($"_tile1 x = {_tile1.x}, y = {_tile1.y}\n" +
+            $"_tile2 x = {_tile2.x}, y = {_tile2.y}");
+
+        var sequence = DOTween.Sequence();
+
+        sequence.Join(_tile1.image.transform.DOMove(_tile2.transform.position, moveDelay))
+                .Join(_tile2.image.transform.DOMove(_tile1.transform.position, moveDelay));
+
+        await sequence.Play().AsyncWaitForCompletion();
+
+        _tile1.image.transform.SetParent(_tile2.transform);
+        _tile2.image.transform.SetParent(_tile1.transform);
+
+        Image tmpImage = _tile1.image;
+        _tile1.image = _tile2.image;
+        _tile2.image = tmpImage;
+
+        selectedTiles.Clear();
     }
 }
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -4,19 +4,47 @@ using UnityEngine.UI;
 public class Tile : MonoBehaviour
 {
     public Image image;
-    [HideInInspector]
+    [SerializeField]
+    private Button button;
+
+    //[HideInInspector]
     public int x;
-    [HideInInspector]
+    //[HideInInspector]
     public int y;
     [HideInInspector]
     public Item item;
 
+    //[HideInInspector]
+    public Tile top;
+    //[HideInInspector]
+    public Tile bottom;
+    //[HideInInspector]
+    public Tile left;
+    //[HideInInspector]
+    public Tile right;
+
+    public Tile[] checkTiles;
+    
+    public void SetTile(Item _item)
+    {
+        item = _item;
+        image.sprite = _item.image;
+    }
     public void SetTile(int _x, int _y, Item _item)
     {
         x = _x;
         y = _y;
-        item = _item;
 
+        item = _item;
         image.sprite = _item.image;
+
+        button.onClick.AddListener(delegate { Board.instance.OnClickTile(this); });
+
+        top = y == 0 ? null : Board.instance.rows[y - 1].tiles[x];
+        bottom = y == Board.instance.rows.Length - 1 ? null : Board.instance.rows[y + 1].tiles[x];
+        left = x == 0 ? null : Board.instance.rows[y].tiles[x - 1];
+        right = x == Board.instance.rows[y].tiles.Length - 1 ? null : Board.instance.rows[y].tiles[x + 1];
+
+        checkTiles = new Tile[] { top, bottom, left, right };
     }
 }
